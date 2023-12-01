e61923e02c13b28962bc4f6eafba389356974551
Kristian Orstad <kristian.orstad@gmail.com>
2023-12-01 13:26:16 +0100

    initial commit

---
+++ snake.py
@@ -0,0 +1,46 @@
+
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]  # konstanter har alltid store bokstaver
+MOVE_DISTANCE = 20
+
+class Snake: # stor bokstav på klassenavn
+
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        #add a new segment to the snake
+        self.add_segment(self.segments[-1].position())
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+
+
+    def up(self):
+        self.segments[0].setheading(90)
+    def down(self):
+        self.segments[0].setheading(270)
+    def left(self):
+        self.segments[0].setheading(180)
+    def right(self):
+        self.segments[0].setheading(0)
